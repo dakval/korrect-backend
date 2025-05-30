a3f139ed91acf215ee9a254b277507e3114ef644
dakval <v_v0412@naver.com>
2025-05-30 22:04:53 +0900

    hhh

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+import os
+from dotenv import load_dotenv
 import google.generativeai as genai
 from sentence_transformers import SentenceTransformer, util
 import torch
@@ -8,27 +10,25 @@ import numpy as np
 import pickle
 import re
 
-import os
-from dotenv import load_dotenv
+# 환경 변수 로딩
 load_dotenv()
+API_KEY = os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=API_KEY)
+gemini_model = genai.GenerativeModel(model_name="models/gemini-2.0-flash")
 
+# 서버 인스턴스
 app = FastAPI()
 
 # CORS 허용
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # 실제 배포시 제한해도 됨
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# 환경 변수
-API_KEY = os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key=API_KEY)
-gemini_model = genai.GenerativeModel(model_name="models/gemini-2.0-flash")
-
-# 모델 및 데이터 로딩
+# 데이터 로딩
 with open("rag_chunks.pkl", "rb") as f:
     chunks = pickle.load(f)
 
@@ -63,18 +63,4 @@ def correct_text(item: SentenceInput):
         final_prompt = (
             "다음 문장과 문단을 참고해서 맞춤법을 교정해 주세요. temperature를 고려해서 문맥과 말투를 유지하고, 신조어나 틀리지 않은 단어는 그대로 두세요.\n\n"
             + "\n\n".join(prompt_sentences)
-            + "\n\n각 문장에 대해 '문장:'으로 시작하는 줄마다 하나씩 교정해 주세요. 문장의 순서는 번호 순서와 같게 해 주세요. 답변은 번호는 매기지 말고 '문장 :'을 제거하고 쭉 이어서 답해 주세요."
-        )
-
-        response = gemini_model.generate_content(
-            final_prompt,
-            generation_config=genai.types.GenerationConfig(temperature=0.0)
-        )
-
-        if response and response.text:
-            return {"corrected": response.text}
-        else:
-            return {"corrected": "⚠️ 교정 결과를 받아오지 못했습니다."}
-
-    except Exception as e:
-        return {"corrected": f"❌ 오류 발생: {str(e)}"}
+            + "\n\n각 문장에 대해 '문장:'으로 시작하는 줄마다 하나씩 교정해 주세요. 문장의 순서는 번호 순서와 같게 해 주세요. 답변은 번호는 매기지 말고 '문장 :
