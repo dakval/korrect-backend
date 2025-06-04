03b74e725ad0447f7f7dbb40d0239b0ed905db2c
dakval <v_v0412@naver.com>
2025-06-05 01:04:15 +0900

    11

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ def correct_text(item: SentenceInput):
         elif mode == "casual":
             final_prompt = (
                 "다음 문장과 문단을 참고하여 띄어쓰기와 맞춤법을 원칙에 기반하여 교정해 주세요. "
-                # "temperature를 고려해서 문맥과 말투를 유지해 주세요. "
+                "temperature를 고려해서 말투는 건들지 말아 주세요. "
                 # "신조어나 틀리지 않은 단어는 그대로 두세요.\n\n"
                 + "\n\n".join(prompt_sentences)
                 + "\n\n각 문장에 대해 하나씩 교정해 주세요. "
